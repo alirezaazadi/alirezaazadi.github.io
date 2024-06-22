db35d0ffe8d48e57ecf8de2ab994ca58e3e488f1
Alireza Azadi <Alireza_Azadi@Hotmail.com>
2024-06-22 20:43:00 +0200

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+My personal blog repository: [https://a-azadi.blog](https://a-azadi.blog)
+
+I forked this repository from [https://github.com/morethanmin/morethan-log](morethanmin/morethan-log) and made some
+changes to it.
---
+++ site.config.js
@@ -0,0 +1,74 @@
+const CONFIG = {
+  // profile setting (required)
+  profile: {
+    name: "Alireza Azadi",
+    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "back-end developer",
+    bio: "An observer, a sojourner, an Ibn us-Sabiil.\nHere, I write about whatever amuses me; Share my thoughts, and experiences.",
+    email: "Alireza_Azadi@Hotmail.com",
+    linkedin: "a-azadi",
+    github: "alirezaazadi",
+    instagram: "alireza_aazadi",
+  },
+  projects: [
+    {
+      name: `Cehckout my github`,
+      href: "https://github.com/alirezaazadi",
+    },
+  ],
+  // blog setting (required)
+  blog: {
+    title: "Alireza Azadi's Blog",
+    description: "I write about whatever amuses me. Share my thoughts, experiences.",
+  },
+
+  // CONFIG configration (required)
+  link: "https://a-azadi.blog",
+  since: 2024, // If leave this empty, current year will be used.
+  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
+
+  // notion configuration (required)
+  notionConfig: {
+    pageId: process.env.NOTION_PAGE_ID,
+  },
+
+  // plugin configuration (optional)
+  googleAnalytics: {
+    enable: false,
+    config: {
+      measurementId: process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID || "",
+    },
+  },
+  googleSearchConsole: {
+    enable: false,
+    config: {
+      siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
+    },
+  },
+  naverSearchAdvisor: {
+    enable: false,
+    config: {
+      siteVerification: process.env.NEXT_PUBLIC_NAVER_SITE_VERIFICATION || "",
+    },
+  },
+  utterances: {
+    enable: true,
+    config: {
+      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
+      "issue-term": "og:title",
+      label: "💬 Utterances",
+    },
+  },
+  cusdis: {
+    enable: false,
+    config: {
+      host: "https://cusdis.com",
+      appid: "", // Embed Code -> data-app-id value
+    },
+  },
+  isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
+  revalidateTime: 21600 * 7, // revalidate time for [slug], index
+}
+
+module.exports = { CONFIG }
